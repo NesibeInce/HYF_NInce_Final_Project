5595accd00be68f8e8181d4b9385b319f40c5b54
NesibeInce <nesybe.ince@gmail.com>
2020-03-01 03:12:58 +0100

    NInce_Final_Project_Wk2

---
+++ NODE-EXPRESS_WK2/project_0/mongoose.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const Product = require('./models/products');
+
+mongoose
+  .connect(
+    'mongodb+srv://nesy:R6Ie1ZdYx2XOJ37X@cluster0-3lgpf.mongodb.net/test?retryWrites=true&w=majority',
+  )
+  .then(() => {
+    console.log('Connected to database!');
+  })
+  .catch(() => {
+    console.log('Connection failed!');
+  });
+
+const createProduct = async (req, res, next) => {
+  const createdProduct = new Product({
+    name: req.body.name,
+    price: req.body.price,
+  });
+  const result = await createdProduct.save();
+
+  res.json(result);
+};
+
+const getProducts = async (req, res, next) => {
+  const products = await Product.find().exec();
+  res.json(products);
+};
+
+exports.createProduct = createProduct;
+exports.getProducts = getProducts;
